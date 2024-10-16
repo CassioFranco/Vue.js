90fbc6eeec6ada99c548a77433ed1a2fee9d22bc
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-16 16:39:08 -0300

    Emit

---
+++ Vue Project/src/components/Mudarimagem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('mudarimagem')">Trocar imagem de perfil</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Mudarimagem",
+        emits: ['mudarimagem']
+    }
+</script>
--- Vue Project/src/components/Picture.vue
+++ Vue Project/src/components/Picture.vue
@@ -1,15 +1,37 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao" width="200px">
+        <Mudarimagem @mudarimagem="trocarimagem" />
+    </div>
 </template>
 
 <script>
-    export default{
-        name:'Picture',
-        data(){
-            return{
-                avatar: '/public/img/ghost-custom.png',
-                descricao: 'banana'
+import Mudarimagem from './Mudarimagem.vue';
+
+export default {
+    name: 'Picture',
+    components: {
+        Mudarimagem
+    },
+    data() {
+        return {
+            avatar: '/public/img/ghost-custom.png',
+            descricao: 'scream',
+            isGhost: true
+        }
+    },
+    methods: {
+        trocarimagem() {
+            if (this.isGhost) {
+                this.avatar = '/public/img/vampire.png';
+                this.descricao = 'vampire';
+            } else {
+                this.avatar = '/public/img/ghost-custom.png';
+                this.descricao = 'scream';
             }
+            this.isGhost = !this.isGhost;
+            console.log("Imagem alternada");
         }
     }
+}
 </script>
