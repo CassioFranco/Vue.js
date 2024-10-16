6ee81613bca6b6d79233e619445a3b306bb88956
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-16 15:43:55 -0300

    Reutilização de componentes

--- Vue Project/src/components/Form.vue
+++ Vue Project/src/components/Form.vue
@@ -33,6 +33,7 @@ export default {
         }
     },
 
+        
     methods: {
         enviarFormulario(e) {
 
--- Vue Project/src/components/PrimeiroComponent.vue
+++ Vue Project/src/components/PrimeiroComponent.vue
@@ -3,17 +3,20 @@
         <h1>Olá vue!</h1>
         <p> Meu Nome é {{ nome }} e trabalho como {{ profissao }}</p>
         <MultiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
 
-    import MultiplosEventos from "./MultiplosEventos.vue"
+    import MultiplosEventos from "./MultiplosEventos.vue";
+    import Reutilizacao from "./Reutilizacao.vue";
 
     export default{
         name: "PrimeiroComponent",
         components:{
-            MultiplosEventos
+            MultiplosEventos,
+            Reutilizacao
         },
         data() {
             return {
