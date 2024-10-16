a58541bbc0054b024f6fda3ca2a312a6074d71d2
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-16 13:59:27 -0300

    Múltiplos eventos

--- Vue Project/src/components/Form.vue
+++ Vue Project/src/components/Form.vue
@@ -1,28 +1,61 @@
 <template>
     <div>
         <h2>Meu Formulário:</h2>
-    <form action="">
-        <div>
-            <inputText />
-        </div>
-        <div>
-            <inputText />
-        </div>
-        <div>
-            <submit/>
-        </div>
-    </form>
+        <form action="" @submit="enviarFormulario($event)">
+            <div>
+                <input type="text" v-model="name">
+            </div>
+            <div>
+                <input type="text" v-model="email">
+            </div>
+            <div>
+                <submit />
+            </div>
+        </form>
+        <p>Conteúdos</p>
+        <p>Conteúdos</p>
+        <p>Conteúdos</p>
     </div>
 </template>
 <script>
 import submit from './form/submit.vue';
 import inputText from './form/inputText.vue';
-    export default {
-        name: 'Form',
-        components: {
-            inputText,
-            submit
+export default {
+    name: 'Form',
+    components: {
+        inputText,
+        submit
+    },
+    data() {
+        return {
+            name: "",
+            email: ""
+        }
+    },
+
+    methods: {
+        enviarFormulario(e) {
+
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log("Formulário Enviado!");
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+
+            // Ajax
+
+            // inserir no banco os dados
         }
     }
+}
 
 </script>
+
+<style scoped>
+p {
+    margin: 50px;
+}
+</style>
--- Vue Project/src/components/PrimeiroComponent.vue
+++ Vue Project/src/components/PrimeiroComponent.vue
@@ -2,12 +2,19 @@
     <div>
         <h1>Olá vue!</h1>
         <p> Meu Nome é {{ nome }} e trabalho como {{ profissao }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
-<script>    
+<script>
+
+    import MultiplosEventos from "./MultiplosEventos.vue"
+
     export default{
         name: "PrimeiroComponent",
+        components:{
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: 'Cássio',
